279628c2c13bd6d3b5a0b02dca5354ae055ee37c
HarpinderSingh1999 <tui57086@temple.edu>
2025-03-01 21:31:11 -0500

    made sure the buttons were like dynamic so all we have to do is add stuff ot the stories, i also then delted the add word button and next sentence button. For now I dont think that will slow Molly down with the use-sound library since i think she is building it based off what the screen shows and we could literally just have a slow method. Also the news buttons work with use-sound!

--- StoryQuest/.next/static/chunks/webpack.js
+++ StoryQuest/.next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames based on template
-/******/ 			return undefined;
+/******/ 			return "static/chunks/" + chunkId + ".js";
 /******/ 		};
 /******/ 	})();
 /******/ 	
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("99080b6c2d70d84c")
+/******/ 		__webpack_require__.h = () => ("c9ef5142ac4695f5")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
